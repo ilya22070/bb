5754037c95b35b9507af50ddeebc71256eee2314
ilya22070 <62645091+ilya22070@users.noreply.github.com>
2020-11-09 12:31:24 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,52 @@
+let scrollPos = window.pageYOffset,
+  header = document.querySelector('.header'),
+  headerHeight = header.offsetHeight,
+  fixed = document.querySelector('.fixed')
+
+window.addEventListener('scroll', () => {
+  scrollPos = this.pageYOffset
+  if (scrollPos >= headerHeight) {
+    fixed.style.position = 'fixed'
+    fixed.style.top = '0px'
+  } else {
+    fixed.style.position = 'absolute'
+    fixed.style.top = '-100px'
+  }
+})
+
+let lScroll = document.querySelectorAll('.scroll__links')
+
+lScroll.forEach((e) => {
+  let elementID = e.dataset.scroll
+  let elementOffset = document.querySelector(elementID).offsetTop
+
+  if (e.closest('div').classList.contains('burger__menu-item')) {
+    var burgerLink = e
+  }
+  e.addEventListener('click', function (e) {
+    if (burgerLink) {
+      burger.style.right = '-100%'
+    }
+    e.preventDefault()
+    window.scrollTo({
+      top: elementOffset - 20,
+      behavior: 'smooth',
+    })
+  })
+})
+
+let burgerBtn = document.querySelectorAll('.burger__btn'),
+  burgerClose = document.querySelectorAll('.burger__btn-close'),
+  burger = document.querySelector('.burger__menu')
+
+burgerBtn.forEach((e) => {
+  e.addEventListener('click', () => {
+    burger.style.right = '0'
+  })
+})
+
+burgerClose.forEach((e) => {
+  e.addEventListener('click', () => {
+    burger.style.right = '-100%'
+  })
+})
